448f1a622eb5e960c200ba22c702231dcb64688e
Mohammad Taslim <55713524+MohammedTaslim@users.noreply.github.com>
2020-05-27 00:14:26 +0530

    all files

---
+++ SpringExample-1/src/main/java/com/tas/service/ShoppingCartService.java
@@ -0,0 +1,20 @@
+package com.tas.service;
+
+import com.tas.exception.NotEnoughProductsInStockException;
+import com.tas.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface ShoppingCartService {
+
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    Map<Product, Integer> getProductsInCart();
+
+    void checkout() throws NotEnoughProductsInStockException;
+
+    BigDecimal getTotal();
+}
